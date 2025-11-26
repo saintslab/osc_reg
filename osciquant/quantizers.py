3e3917e2c36d2df9f5337b686cceef4cc3c760a1
Jonathan Wenshøj <jonathanjonathanw@gmail.com>
2025-11-26 22:19:37 +0100

    Update quantizers.py

--- osciquant/quantizers.py
+++ osciquant/quantizers.py
@@ -19,22 +19,3 @@ class UniformQuantizer(nn.Module):
         s = max_val / (2 ** (self.bit_width - 1) - 1)
 
         return RoundSTE.apply(x / s) * s
-
-    
-class LSQQuantizer(nn.Module):
-    def __init__(self, bit_width, init_scale=1.0):
-        super().__init__()
-        self.bit_width = bit_width
-        self.s = nn.Parameter(torch.tensor(init_scale))
-
-    def forward(self, X):
-        s = torch.abs(self.s)  # ensure scale is always positive
-        Qn = - (2 ** (self.bit_width - 1))  # Min q level
-        Qp = (2 ** (self.bit_width - 1)) - 1  # Max q level
-
-        X_scaled = X / s
-        X_clamped = torch.clamp(X_scaled, Qn, Qp)
-        X_rounded = RoundSTE.apply(X_clamped) 
-        X_quantized = X_rounded * s
-
-        return X_quantized
